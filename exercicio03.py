01628693d223b4098fca20fbad5b5e3d0e72822b
bela <belavictoria10@gmail.com>
2025-03-31 14:59:38 -0300

    Add as 4 operações

---
+++ exercicio03.py
@@ -0,0 +1,7 @@
+n1 =float(input("Digite o primeiro numero:"))
+n2 =float(input("Digite o segundo numero:"))
+soma = n1 + n2
+subtracao = n1 - n2
+divisao = n1 / n2
+multiplicacao = n1 * n2
+print(f"Resultado da soma:{soma}; subtração:{subtracao}; divisão{divisao}; multiplicação{multiplicacao}")
