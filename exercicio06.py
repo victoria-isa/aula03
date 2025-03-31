0e771061383ea56a61ff0d545de07f5a1132cb84
bela <belavictoria10@gmail.com>
2025-03-31 16:52:54 -0300

    Add medias de recuperação

--- exercicio06.py
+++ exercicio06.py
@@ -2,8 +2,10 @@ n1 =float(input("Qual é a primeira nota?:"))
 n2 =float(input("Qual é a segunda nota?:"))
 n3 =float(input("Qual é a terceira nota?:"))
 media = float(n1 + n2 + n3) / 3
-print(f"A média desse aluno é: {media}")
+print(f"A média desse aluno é: {media:.2f}")
 if media >= 7:
     print("aluno aprovado")
-else:
+if media< 4:
     print("aluno reprovado")
+else:
+    print("aluno recuperação")
